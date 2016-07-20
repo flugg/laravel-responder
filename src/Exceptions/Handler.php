febc05df5d1ae66ae3e1cc857ba388c90b20efb2
Flugger <flugged@gmail.com>
2016-07-20 19:17:37 +0200

    Add forgotten exception handler

---
+++ src/Exceptions/Handler.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Flugg\Responder\Exceptions;
+
+use Exception;
+use Flugg\Responder\Traits\HandlesApiErrors;
+use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+
+class Handler extends ExceptionHandler
+{
+    use HandlesApiErrors;
+
+    /**
+     * Render an exception into an HTTP response.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @param  Exception                $e
+     * @return \Illuminate\Http\Response
+     */
+    public function render( $request, Exception $e )
+    {
+        if ( $e instanceof ApiException ) {
+            return $this->renderApiErrors( $e );
+        }
+
+        return parent::render( $request, $e );
+    }
+}
